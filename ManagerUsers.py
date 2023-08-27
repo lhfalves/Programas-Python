be215940218c273b9d7a371b8e82c24883b11adc
lhfalves <95190141+lhfalves@users.noreply.github.com>
2023-08-27 17:28:03 -0300

    Update ManagerUsers.py

--- ManagerUsers.py
+++ ManagerUsers.py
@@ -26,9 +26,6 @@ def registro():
 
 def inserir(dicionario):
 
-     #dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
-     #                                                    input("Digite a última data de acesso: "),
-     #                                                    input("Qual a última estação acessada: ").upper()]
     teste = True
     while teste:
         login = input("Digite o login: ").upper()
